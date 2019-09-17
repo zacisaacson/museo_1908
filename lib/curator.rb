8b568a22b3a62055a19e5da20b1861774535db50
Zac Isaacson <isaacson.zm@gmail.com>
2019-09-17 10:52:36 -0600

    Complete iteration 3

--- lib/curator.rb
+++ lib/curator.rb
@@ -26,5 +26,30 @@ class Curator
     end
   end
 
+  def find_photographs_by_artist(artist)
+    @photographs.find_all do |photo|
+      artist.id == photo.artist_id
+    end
+  end
+
+  def artists_with_multiple_photographs
+    @artists.find_all do |artist|
+      find_photographs_by_artist(artist).count > 1
+    end
+  end
+
+  def photographs_taken_by_artist_from(country)
+    artists_from = @artists.find_all do |artist|
+      country == artist.country
+    end
+      photos_from = []
+      artists_from.each do |artist|
+      photos_from << find_photographs_by_artist(artist)
+    end
+    photos_from.flatten
+  end
+
+
+
 
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -19,6 +19,18 @@ class CuratorTest < Minitest::Test
          artist_id: "2",
          year: "1941"
     })
+    @photo_3 = Photograph.new({
+         id: "3",
+         name: "Identical Twins, Roselle, New Jersey",
+         artist_id: "3",
+         year: "1967"
+    })
+    @photo_4 = Photograph.new({
+         id: "4",
+         name: "Monolith, The Face of Half Dome",
+         artist_id: "3",
+         year: "1927"
+    })
     @artist_1 = Artist.new({
         id: "1",
         name: "Henri Cartier-Bresson",
@@ -32,6 +44,13 @@ class CuratorTest < Minitest::Test
         born: "1902",
         died: "1984",
         country: "United States"
+    })
+    @artist_3 = Artist.new({
+         id: "3",
+         name: "Diane Arbus",
+         born: "1923",
+         died: "1971",
+         country: "United States"
     })
   end
 
@@ -76,6 +95,43 @@ class CuratorTest < Minitest::Test
     @curator.add_photograph(@photo_2)
 
     assert_equal @photo_2, @curator.find_photograph_by_id("2")
+  end
+
+  def test_find_photos_by_artist
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+
+    assert_equal [@photo_3, @photo_4], @curator.find_photographs_by_artist(@artist_3)
+  end
+
+  def test_artists_with_multiple_photos
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+
+    assert_equal [@artist_3], @curator.artists_with_multiple_photographs
+  end
+
+  def test_photos_taken_by_artist_from
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+
+    assert_equal [@photo_2, @photo_3, @photo_4], @curator.photographs_taken_by_artist_from("United States")
+    assert_equal [], @curator.photographs_taken_by_artist_from("Argentina")
 
   end
 
